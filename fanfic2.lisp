d27c0490c1e9968811258da12a5eb724499a31cf
Henry Poggie <henry.poggie@gmail.com>
2019-06-24 22:43:25 -0400

    Handle non-tag lists properly

--- fanfic2.lisp
+++ fanfic2.lisp
@@ -36,7 +36,7 @@ NOTE: assumes that the tree is in fact a tree, with no back edges."
                             (list (if (equalp name tagname) tree))
                             (reduce #'append
                                     (mapcar (lambda (element) (find-tags-recursive tagname element))
-                                            (remove-if-not #'listp content)))))))
+                                            (filter-match (list* _ _) content)))))))
 
 (defun find-tags (tagname tree)
   ;; 1st element is the tag, 2nd is args. 3rd is what we want
