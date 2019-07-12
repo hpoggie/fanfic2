feec66c77236f74bdc03c06261bd5d4710b852cc
Henry Poggie <henry.poggie@gmail.com>
2019-07-12 08:19:23 -0400

    Add pages

--- dir.lisp
+++ dir.lisp
@@ -27,3 +27,24 @@
    (remove-if #'not)
    (mapcar (lambda (x)
              (concatenate 'string "https://www.fanfiction.net" x)))))
+
+(defun num-pages (url)
+  (-<>>
+   (grab url)
+   (find-tags-recursive "a")
+   (remove-if-not (lambda (x) (equalp (third x) "Last")))
+   (first)
+   (trivia:match <> ((list _ (list (list _ url)) _) url))
+   (cl-strings:split <> "=")
+   (last)
+   (first)
+   (parse-integer)))
+
+(defun pages (url)
+  "Find the individual pages for the URL. Returns them in reverse numerical order."
+  (let ((max (num-pages url)))
+    ;; This may look like gibberish but I swear it makes sense
+    ;; ~a means "substitute a thing here", "?&p=" tells ffnet what page to get
+    ;; So it would expand to something like https://www.fanfiction.net/anime/Naruto/?&p=2
+    (loop for i from 1 to max collect
+                              (format nil "~a?&p=~a" url i))))
