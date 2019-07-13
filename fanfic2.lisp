f97fbea984db27342b6a6c97f01a4b96921adfc2
Henry Poggie <henry.poggie@gmail.com>
2019-07-12 20:07:28 -0400

    Get fics from all pages

--- fanfic2.lisp
+++ fanfic2.lisp
@@ -36,6 +36,8 @@ and extract the fic descriptions"
    (remove-if (lambda (x) (member x '("https://www.fanfiction.net/anime/What-do-you-do-at-the-end-of-the-world-Are-you-busy-Will-you-save-us-%E7%B5%82%E6%9C%AB%E3%81%AA%E3%81%AB%E3%81%97%E3%81%A6%E3%81%BE%E3%81%99%E3%81%8B-%E5%BF%99%E3%81%97%E3%81%84%E3%81%A7%E3%81%99%E3%81%8B-%E6%95%91%E3%81%A3%E3%81%A6%E3%82%82%E3%82%89%E3%/"
                                       "https://www.fanfiction.net/tv/Law-Order-Division-of-Field-Investigation-%D0%97%D0%B0%D0%BA%D0%BE%D0%BD-%D0%B8-%D0%BF%D0%BE%D1%80%D1%8F%D0%B4%D0%BE%D0%BA-%D0%BE%D1%82%D0%B4%D0%B5%D0%BB-%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D1%8B%D1%85-%D1%80%D0%B0%D1%81%D1%81%D0%BB%D0%/")
                                   :test #'equalp)))
+   (mapcar #'pages)
+   (reduce #'append)
    (mapcar #'worst-of-page)
    ;; If there was an error, don't use the page
    (remove-if-not #'first)
